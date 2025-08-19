04df05fee627ebbc5eb9ac307a745f848ed43f73
Michael <tangm0211@icloud.com>
2025-08-19 14:43:21 -0500

    Added controller, model, and route for messages.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "/routes/message.routes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDatabase } from "./lib/database.js";
 
 dotenv.config();
@@ -9,8 +11,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 app.listen(PORT, () => {
     console.log("server is running on PORT: "  + PORT);
     connectDatabase();
